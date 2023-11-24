7d67d05a07a44acd05e895fef61e83378d82f5a1
Jack Dunn <56315967+JackDunnCodes@users.noreply.github.com>
2023-11-24 18:58:01 +0000

    Done for tonight

--- source/main.cpp
+++ source/main.cpp
@@ -8,43 +8,39 @@ MicroBit uBit;
 static uint8_t locount = 0;
 static uint8_t hicount = 0;
 
+// struct FB_S
+//     {
+//         uint8_t         length;                             // The length of the remaining bytes in the packet. includes protocol/version/group fields, excluding the length field itself.
+//         uint8_t         version;                            // Protocol version code.
+//         uint8_t         group;                              // ID of the group to which this packet belongs.
+//         uint8_t         protocol;                           // Inner protocol number c.f. those issued by IANA for IP protocols
+
+//         uint8_t         payload[MICROBIT_RADIO_MAX_PACKET_SIZE];    // User / higher layer protocol data
+//         FrameBuffer     *next;                              // Linkage, to allow this and other protocols to queue packets pending processing.
+//         int             rssi;                               // Received signal strength of this frame.
+//     } typedef FrameBuffer;
+
+FrameBuffer *rxBuf;
+int *sample;
+
 extern "C" void RADIO_IRQHandler(void)
 {
-    if(NRF_RADIO->EVENTS_TXREADY)
+    // immediately set off timer, hopefully improve determinism
+    NRF_TIMER0->PRESCALER = 0;
+    NRF_TIMER0->CC[0] = 3200; // 200 microseconds
+    NRF_TIMER0->TASKS_START=1;
+    if(NRF_RADIO->EVENTS_CRCOK)
     {
-        NRF_RADIO->EVENTS_TXREADY = 0;
-        NRF_RADIO->EVENTS_END = 0;
-
-        NRF_RADIO->SHORTS &= ~RADIO_SHORTS_DISABLED_TXEN_Msk;
-        NRF_RADIO->SHORTS |=  RADIO_SHORTS_END_RXEN_Msk;
-
-        NRF_RADIO->TASKS_START = 1;
-    }
-    if(NRF_RADIO->EVENTS_RXREADY)
-    {
-        NRF_RADIO->EVENTS_RXREADY = 0;
-        NRF_RADIO->SHORTS &= ~RADIO_SHORTS_END_RXEN_Msk;
-        NRF_RADIO->SHORTS |=  RADIO_SHORTS_DISABLED_TXEN_Msk;
-
-        // Start listening and wait for the END event
-        NRF_RADIO->TASKS_START = 1;
-    }
-
-    if(NRF_RADIO->EVENTS_END)
-    {
-        NRF_RADIO->EVENTS_END = 0;
+        NRF_RADIO->EVENTS_CRCOK = 0;
         if(NRF_RADIO->CRCSTATUS == 1)
         {
-            int sample = (int)NRF_RADIO->RSSISAMPLE;
-
-            // Associate this packet's rssi value with the data just
-            // transferred by DMA receive
-            MicroBitRadio::instance->setRSSI(-sample);
+            *sample = (int)NRF_RADIO->RSSISAMPLE;
 
             // Now move on to the next buffer, if possible.
             // The queued packet will get the rssi value set above.
-            MicroBitRadio::instance->queueRxBuf();
+            rxBuf = NRF_RADIO->
 
+            
             // Set the new buffer for DMA
             NRF_RADIO->PACKETPTR = (uint32_t) MicroBitRadio::instance->getRxBuf();
         }
@@ -53,8 +49,49 @@ extern "C" void RADIO_IRQHandler(void)
             MicroBitRadio::instance->setRSSI(0);
         }
 
+        
+        NRF_RADIO->TASKS_DISABLE = 1;
+    } else {
+        // cancel timer
+        NRF_TIMER0->TASKS_STOP = 1;
+        NRF_TIMER0->TASKS_CLEAR = 1;
+    }
+
+    if(NRF_RADIO->EVENTS_TXREADY)
+    {
+        NRF_RADIO->EVENTS_TXREADY = 0;
+        NRF_RADIO->EVENTS_END = 0;
+
+        NRF_RADIO->SHORTS &= ~RADIO_SHORTS_DISABLED_TXEN_Msk;
+        NRF_RADIO->SHORTS |=  RADIO_SHORTS_DISABLED_RXEN_Msk;
+    }
+    if(NRF_RADIO->EVENTS_RXREADY)
+    {
+        NRF_RADIO->EVENTS_RXREADY = 0;
+        NRF_RADIO->SHORTS |=  RADIO_SHORTS_DISABLED_TXEN_Msk;
+        NRF_RADIO->SHORTS &= ~RADIO_SHORTS_DISABLED_RXEN_Msk;
+        NRF_RADIO->PACKETPTR = rxBuf;
+
         // Start listening and wait for the END event
         NRF_RADIO->TASKS_START = 1;
+    }
+
+    
+    if(NRF_RADIO->EVENTS_PHYEND)
+    {
+        NRF_RADIO->EVENTS_PHYEND = 0;
+        NRF_RADIO->TASKS_DISABLE = 1;
+    }
+}
+
+extern "C" void TIMER_IRQhandler(void)
+{
+    if(NRF_TIMER0->EVENTS_COMPARE[0])
+    {
+        NRF_TIMER0->EVENTS_COMPARE[0] = 0;
+        NRF_RADIO->TASKS_START = 1;
+        NRF_TIMER0->TASKS_STOP = 1;
+        NRF_TIMER0->TASKS_CLEAR = 1;
     }
 }
 
@@ -80,6 +117,13 @@ static void onData(MicroBitEvent)
 
 int main()
 {
+    NVIC_SetVector(IRQn_Type::TIMER0_IRQn, TIMER_IRQhandler);
+    NVIC_SetPriority(IRQn_Type::TIMER0_IRQn, 2);
+    NVIC_EnableIRQ(IRQn_Type::TIMER0_IRQn);
+
+    NVIC_SetVector(IRQn_Type::RADIO_IRQn, RADIO_IRQHandler);
+    NVIC_SetPriority(IRQn_Type::RADIO_IRQn, 2);
+    NVIC_EnableIRQ(IRQn_Type::RADIO_IRQn);
     uBit.init();
     uBit.messageBus.listen(DEVICE_ID_RADIO, MICROBIT_RADIO_EVT_DATAGRAM, onData);
     uBit.radio.enable();
